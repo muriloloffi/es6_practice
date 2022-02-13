c57378bc1c2c6bbce949c7dbd4dbddf6e4631fae
Murilo Loffi <muriloloffi@gmail.com>
2022-02-13 20:32:03 -0300

    [lista-negociacoes] prototipo de listagem

--- client/js/app/controllers/NegociacaoController.js
+++ client/js/app/controllers/NegociacaoController.js
@@ -6,21 +6,30 @@ class NegociacaoController {
     this._inputData = $('#data');
     this._inputQuantidade = $('#quantidade');
     this._inputValor = $('#valor');
+    this._listaNegociacoes = new ListaNegociacoes();
   }
 
   adiciona(event) {
 
     event.preventDefault();
+    this._listaNegociacoes.adiciona(this._criaNegociacao());
+    this._limpaFormulario();
+  }
 
-    let negociacao = new Negociacao(
+  _criaNegociacao() {
+
+    return new Negociacao(
       DateHelper.textoParaData(this._inputData.value),
       this._inputQuantidade.value,
-      this._inputValor.value
-    );
+      this._inputValor.value);
+  }
 
-    console.log(negociacao);
+  _limpaFormulario() {
 
-    console.log(DateHelper.dataParaTexto(negociacao.data));
+    this._inputData.value = '';
+    this._inputQuantidade.value = 1;
+    this._inputValor.value = 0.0;
+    this._inputData.focus();
   }
 
   //Por que let logger = console.log.bind(console); não faz diferença?
